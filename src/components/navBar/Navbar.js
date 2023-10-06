5750177f1d0c7e60226810ba0e192eea25fc1b7d
vvvkks <kurilova2003@gmail.com>
2023-10-06 20:09:14 +0300

    add: styles for components

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
+import s from "./Navbar.module.css"
 
 const Navbar = () => {
     return (
-        <div className={"navbar"}>
-            <div className={"container"}>
-                <div className={"logo"}>
-                    <h3>Butcher Without Acid Blog</h3>
+        <div className={s.navbar}>
+            <div className={s.container}>
+                <div className={"s.logo"}>
+                    <h4>butcher without acid <br/><span className="blog">blog</span></h4>
                 </div>
-                <div className={"links"}>
+                <div className={s.links}>
                     <nav>
                         <ul>
-                            <li><a href="/Login">Login</a></li>
-                            <li><a href="/Register">Register</a></li>
+                            <li><a href="/Write">Create new post</a></li>
+                            <li><a href="/Logout">Logout(vvvkks)</a></li>
                         </ul>
                     </nav>
                 </div>
