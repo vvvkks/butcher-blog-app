5d26be84c21795d6b0215bca18ac615f7f72d8c6
vvvkks <kurilova2003@gmail.com>
2023-10-06 17:34:44 +0300

    refactor: Split code into components to improve project structure.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <blockquote>
+                <p><em>Господи, пускай в 2018 году снизят цены на рыболовные снасти</em></p>
+                <p>- Виктория Курилова</p>
+            </blockquote>
+            <section>
+                <h2>Формула успеха</h2>
+                <p>Успех = <sup>Только мы сами можем ловить кринж с себя </sup>&frasl;<sub>Вы не можете.</sub></p>
+            </section>
+            <pre>
+                        <code>
+                            console.log("Не верущие в Бога такие: ЭЭЭ Должно быть этот блог ведет учёный")
+                        </code>
+                    </pre>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/components/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div className={"auth"}>
+            <h1>Register</h1>
+        </div>
+    )
+}
+
+export default Register
