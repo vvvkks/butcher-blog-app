1a7507ef4bf55aa4023f6b07759bed174dd79f0c
vvvkks <kurilova2003@gmail.com>
2023-10-14 16:32:39 +0300

    fix: problems with rendering

--- src/components/navBar/Header.js
+++ src/components/navBar/Header.js
@@ -5,9 +5,7 @@ const Header = () => {
     return (
         <div className={s.navbar}>
             <div className={s.container}>
-                <div className={"s.logo"}>
                     <h4>butcher without acid <br/><span className="blog">blog</span></h4>
-                </div>
                 <div className={s.links}>
                     <nav>
                         <ul>
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import s from "./Main.module.css"
 
 const Main = () => {
     return (
