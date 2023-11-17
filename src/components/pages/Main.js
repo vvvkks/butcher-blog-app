255fdad5f80f6c5c12905cc83ab2ee0d5b6db07c
vvvkks <kurilova2003@gmail.com>
2023-11-17 11:01:44 +0300

    add: information about author and date

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -16,6 +16,10 @@ const Main = () => {
                     </div>
                     <div className={s.content}>
                         <h1 className="post-title">Типы парней</h1>
+                        <p className={s.info}>
+                            <a href="#/" className={s.author}>vvvkks</a>
+                            <time>2023-11-15 18:14</time>
+                        </p>
                         <p className="Post1-description">
                             Я прожила почти 20 лет и за это время очень много общалась с парнями. Сейчас я убедилась,
                             что 99% мальчиков просто одинаковые, у всех есть определённый список проблем, который не
