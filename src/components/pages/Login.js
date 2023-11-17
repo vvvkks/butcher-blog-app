944d54eeb54db82c7cef5fe18a2f75991b9bbdaf
vvvkks <kurilova2003@gmail.com>
2023-11-17 11:00:45 +0300

    add:login form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import s from "./Login.module.css"
 
 const Login = () => {
     return (
-        <div className={"auth"}>
-            <h1>Login</h1>
+        <div className={s.login}>
+            <form action="">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>Login</button>
+            </form>
         </div>
     )
 }
