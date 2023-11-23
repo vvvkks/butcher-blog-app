0797c6304a862414532147b1abd4aa0bffd50647
vvvkks <kurilova2003@gmail.com>
2023-11-23 09:27:59 +0300

    fix: update tg

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import s from "./Footer.module.css";
 const Footer = () => {
     return (
         <footer className={s.footer}>
-            <span>You can be my sponsor! Feel free to reach out on Telegram: <a href="https://t.me/kkkvvs">@kkkvvs</a></span>
+            <span>You can be my sponsor! Feel free to reach out on Telegram: <a href="https://t.me/nevaleft">@nevaleft</a></span>
             <div className={s.someText}>
                 <p>Thanks for visiting! Don't forget to bring your money.</p>
             </div>
