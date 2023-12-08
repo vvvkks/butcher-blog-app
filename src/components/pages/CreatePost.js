ed52a3a5d486c1e2dbab715693e9686f4826899b
vvvkks <kurilova2003@gmail.com>
2023-12-08 17:20:20 +0300

    add: Material-UI library for login page and create post page

--- src/components/pages/CreatePost.js
+++ src/components/pages/CreatePost.js
@@ -1,15 +1,66 @@
-import React from "react";
-import s from "./CreatePost.module.css";
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField'; // Импортируем компонент TextField из библиотеки MUI
+import TextareaAutosize from '@mui/material/TextareaAutosize'; // Импортируем компонент TextareaAutosize из библиотеки MUI
+import Button from '@mui/material/Button'; // Импортируем компонент Button из библиотеки MUI
+import s from './CreatePost.module.css';
 
 function CreatePost() {
+    const [formData, setFormData] = useState({
+        title: '',
+        summary: '',
+    });
+
+    // Обработчик изменения значений в полях ввода
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    // Обработчик отправки формы
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Проверка на введенное название перед отправкой формы
+        if (!formData.title.trim()) {
+            alert('Please enter a text.');
+            return;
+        }
+    };
+
     return (
         <div className={s.createForm}>
             <h1>New post</h1>
-            <form action="">
-                <input type="text" placeholder="Title" />
-                <textarea placeholder="Summary" />
+            <form onSubmit={handleSubmit}>
+                {/* Поле ввода для заголовка */}
+                <TextField
+                    type="text"
+                    name="title"
+                    label="Title"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    required
+                    value={formData.title}
+                    onChange={handleInputChange}
+                />
+                {/* Многострочное поле ввода для текста поста */}
+                <TextareaAutosize
+                    minRows={3}
+                    name="summary"
+                    placeholder="Summary"
+                    required
+                    value={formData.summary}
+                    onChange={handleInputChange}
+                />
+                {/* Поле для загрузки файла */}
                 <input type="file" />
-                <button>Create</button>
+                {/* Кнопка создания поста */}
+                <Button type="submit" variant="contained" color="primary">
+                    Create
+                </Button>
             </form>
         </div>
     );
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,16 +1,118 @@
-import React from 'react'
-import s from "./Login.module.css"
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Typography from '@mui/material/Typography';
+import s from './Login.module.css';
 
 const Login = () => {
+    // Состояния для хранения данных формы и ошибок
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        rememberMe: false,
+    });
+
+    const [errors, setErrors] = useState({
+        email: '',
+        password: '',
+    });
+
+    // Обработчик изменения значений в полях ввода
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+
+        // Очищаем ошибку при изменении значения
+        setErrors({
+            ...errors,
+            [e.target.name]: '',
+        });
+    };
+
+    // Обработчик изменения состояния чекбокса
+    const handleCheckboxChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.checked,
+        });
+    };
+
+    // Обработчик отправки формы
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Валидация почты
+        if (!formData.email || !/^\S+@\S+\.\S+$/.test(formData.email)) {
+            setErrors({
+                ...errors,
+                email: 'Invalid email format',
+            });
+            return;
+        }
+        console.log('Form submitted:', formData);
+    };
+
     return (
-            <div className={s.login}>
-                <h1>Write your login and password here</h1>
-                <form action="">
-                    <input type="text" placeholder="username"/>
-                    <input type="password" placeholder="password"/>
-                    <button>Login</button>
-                </form>
-            </div>
-    )
-}
-export default Login
+        <div className={s.login}>
+            {/* Заголовок формы */}
+            <Typography variant="h4" align="center" gutterBottom>
+                Login
+            </Typography>
+            {/* Форма с использованием MUI компонентов */}
+            <form onSubmit={handleSubmit}>
+                {/* Поле ввода для электронной почты */}
+                <div>
+                    <TextField
+                        type="text"
+                        name="email"
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        value={formData.email}
+                        onChange={handleChange}
+                        error={Boolean(errors.email)}
+                        helperText={errors.email}
+                    />
+                </div>
+                {/* Поле ввода для пароля */}
+                <div>
+                    <TextField
+                        type="password"
+                        name="password"
+                        label="Password"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        value={formData.password}
+                        onChange={handleChange}
+                        error={Boolean(errors.password)}
+                        helperText={errors.password}
+                    />
+                </div>
+                {/* Чекбокс "Remember Me" */}
+                <div>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Checkbox name="rememberMe" color="primary" checked={formData.rememberMe} onChange={handleCheckboxChange} />}
+                            label="Remember Me"
+                        />
+                    </FormGroup>
+                </div>
+                {/* Кнопка отправки формы */}
+                <div>
+                    <Button type="submit" variant="contained" color="primary">
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
